a2d395d39d9cd4b5051bb98afb99112a3b42676d
Reynaldo Vargas <rdv230@nyu.edu>
2015-07-05 16:21:20 -0400

    added sound to ball

---
+++ Assets/Scripts/basketball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class basketball : MonoBehaviour {
+
+	public AudioClip boink;
+	public AudioSource myAudio;
+	public int score;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	void OnCollisionEnter(Collision collider){
+		//What we want to do next is make it so that the velocity of the basketball is tied to the sound
+		myAudio.PlayOneShot(boink);
+		score++;
+		Debug.Log (score);
+
+	}
+
+}
